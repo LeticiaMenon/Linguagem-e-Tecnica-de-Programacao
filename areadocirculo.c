1e1165037dc7964de23451c69f0966566daba328
Leticia Menon <leticia.menon.dev@gmail.com>
2026-08-12 11:44:59 -0300

    Create areadocirculo.c

---
+++ areadocirculo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define pi 3.141592
+
+int main(int argc, char *argv[]) {
+	
+	float area, raio;
+	 printf ("Insira o raio do circulo");
+	 scanf ("%f", &raio);
+
+	area = pi*(raio*raio);
+	
+	printf("A area do circulo de raio %f = %f", raio, area);
+
+return 0;
+	
+	
+}
