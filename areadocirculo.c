59cf1b24e7b268c15b3444020cbc2099cc3117b9
Leticia Menon <leticia.menon.dev@gmail.com>
2026-08-12 11:42:48 -0300

    Delete areadocirculo.c

--- areadocirculo.c
+++
@@ -1,9 +0,0 @@
-int main(int argc, char *argv[]) {
-	
-	float area, raio;
-	 printf ("Insira o raio do circulo");
-	 scanf ("%f", &raio);
-
-	area = pi*(raio*raio);
-	
-	printf("A area do circulo de raio %f = %f", raio, area);
