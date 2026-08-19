a99cfb0fd8dca99fe9a1c35fd2a1c1bd4e238026
Leticia Menon <leticia.menon.dev@gmail.com>
2026-08-19 11:33:48 -0300

    Refactor listadeexercicios.c with additional features

--- listadeexercicios.c
+++ listadeexercicios.c
@@ -27,6 +27,29 @@ int main(int argc, char *argv[]) {
 	
 	//3
 	
+	int n, resultado, bit_64, bit_32, bit_16, bit_8, bit_4, bit_2;
+	printf ("Coloque o valor para a conversao");
+	scanf("%d", &n);
+	
+	bit_64 = n%2;
+	resultado = n/2;
+	
+	bit_32 = resultado%2;
+	resultado = resultado/2;
+	
+	bit_16 = resultado%2;
+	resultado = resultado/2;
+	
+	bit_8 = resultado%2;
+	resultado = resultado/2;
+	
+	bit_4 = resultado%2;
+	resultado = resultado/2;
+	
+	bit_2 = resultado%2;
+	resultado = resultado/2;
+	
+	printf("O numero %d em binario = %d%d%d%d%d%d%d", n, resultado%2, bit_2, bit_4, bit_8, bit_16, bit_32, bit_64);
 
 	
 	//4
